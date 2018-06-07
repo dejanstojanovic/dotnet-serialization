d0afbb9a4dc000b0ad57c89859c1768ab932e212
Dejan Stojanovic <d.stojanovic@hotmail.com>
2018-06-07 22:07:24 +0400

    Add Thrift

---
+++ src/Serialization.Samples/Serializers/SampleThriftSerializer.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Thrift.Protocol;
+using Thrift.Transport;
+
+namespace Serialization.Samples.Serializers
+{
+    public class SampleThriftSerializer<T> : ISerializer<T> where T : class
+    {
+        public T Deserialize(byte[] arrayToDeserialize)
+        {
+            using (var stream = new MemoryStream(arrayToDeserialize))
+            {
+                TProtocol tProtocol = new TBinaryProtocol(new TStreamTransport(stream, stream));
+
+                return barFooMessageThrift.Read(tProtocol) as T;
+            }
+        }
+
+
+        public byte[] Serialize(T objectToSerialize)
+        {
+            using (var stream = new MemoryStream())
+            {
+
+                TProtocol tProtocol = new TBinaryProtocol(new TStreamTransport(stream, stream));
+                objectToSerialize.Write(tProtocol);
+
+                return stream.ToArray();
+            }
+
+
+        }
+    }
+}
